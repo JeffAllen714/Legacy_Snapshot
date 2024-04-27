26052cff60463d62b26571b4338d86c224254a01
JeffAllen714 <117778021+JeffAllen714@users.noreply.github.com>
2024-04-27 13:57:54 -0500

    Update ec2.tf

--- cloudgoat/scenarios/legacy_snapshot/terraform/ec2.tf
+++ cloudgoat/scenarios/legacy_snapshot/terraform/ec2.tf
@@ -68,7 +68,7 @@ resource "aws_ami" "flag_ami" {
     volume_type = "gp2"  
     delete_on_termination = true  
   }
-  source_ami = "ami-073c5fc1798eb7056" # This is a public Ubuntu AMI
+  # source_ami = "ami-073c5fc1798eb7056" # This is a public Ubuntu AMI
 }
 
 # Upload the SSH key to S3
